35b04f006eadff4501edcef2b5abbf92c7515111
wangjiangwen <wangjw2134@163.com>
2024-05-31 21:54:53 +0800

    init

--- src/main/java/com/wangjiangwen/tool/core/util/PageUtil.java
+++ src/main/java/com/wangjiangwen/tool/core/util/PageUtil.java
@@ -6,10 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PageUtil {
-    public static <S,T> Page<T> copy(List<S> source, Class<T> target){
+    public static <S,T> Page<T> copy(Page<S> source, Class<T> target){
         Page<T> tPage = ObjUtil.copy(source, Page.class);
         List<T> ts = new ArrayList<>();
-        for (S s : source) {
+        for (S s : source.getRecords()) {
             T t = ObjUtil.copy(s, target);
             ts.add(t);
         }
